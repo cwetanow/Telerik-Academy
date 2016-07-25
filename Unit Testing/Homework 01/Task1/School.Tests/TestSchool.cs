fe679597bb848b4eea1ad255aa7bcda3ec470880
Ivan <madritistaaa@gmail.com>
2016-07-25 17:46:24 +0300

    Finish task 1

---
+++ Unit Testing/Homework 01/Task1/School.Tests/TestSchool.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Tests
+{
+    [TestFixture]
+    public class TestSchool
+    {
+        [TestCase]
+        public void School_InitialisesCorrectly()
+        {
+            var school = new School();
+            Assert.IsNotNull(school);
+        }
+    }
+}
