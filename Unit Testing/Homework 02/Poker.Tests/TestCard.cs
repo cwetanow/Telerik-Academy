3c2c19ccb72da38040b1648d4319103fb83f744f
Ivan <madritistaaa@gmail.com>
2016-07-26 22:19:31 +0300

    Wrote parameterized tests for cars

--- Unit Testing/Homework 02/Poker.Tests/TestCard.cs
+++ Unit Testing/Homework 02/Poker.Tests/TestCard.cs
@@ -11,10 +11,14 @@ namespace Poker.Tests
     [TestFixture]
     public class TestCard
     {
-        [Test]
-        public void TestCard_ToStringImplement_ShouldReturnCardFaceAndSuite()
+        [TestCase(CardFace.Ace,CardSuit.Clubs)]
+        [TestCase(CardFace.Queen, CardSuit.Diamonds)]
+        [TestCase(CardFace.Eight, CardSuit.Clubs)]
+        [TestCase(CardFace.Ten, CardSuit.Spades)]
+        [TestCase(CardFace.Two, CardSuit.Hearts)]
+        public void TestCard_ToStringImplement_ShouldReturnCardFaceAndSuite(CardFace face, CardSuit suit)
         {
-            var card = new Card(CardFace.Ace, CardSuit.Clubs);
+            var card = new Card(face, suit);
             var result = string.Format("{0} of {1}", card.Face, card.Suit);
             Assert.AreEqual(result, card.ToString());
         }
