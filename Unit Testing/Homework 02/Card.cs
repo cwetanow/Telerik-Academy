42bd89caffd2da2e77736c986e8b150271ef1152
Ivan <madritistaaa@gmail.com>
2016-07-26 22:16:16 +0300

    Start secont unit testing homework

---
+++ Unit Testing/Homework 02/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Poker
+{
+    public class Card : ICard
+    {
+        public CardFace Face { get; private set; }
+        public CardSuit Suit { get; private set; }
+
+        public Card(CardFace face, CardSuit suit)
+        {
+            this.Face = face;
+            this.Suit = suit;
+        }
+
+        public override string ToString()
+        {
+           return string.Format("{0} of {1}", this.Face,this.Suit);
+        }
+    }
+}
---
+++ Unit Testing/Homework 02/Poker.Tests/TestCard.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Poker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker.Tests
+{
+    [TestFixture]
+    public class TestCard
+    {
+        [Test]
+        public void TestCard_ToStringImplement_ShouldReturnCardFaceAndSuite()
+        {
+            var card = new Card(CardFace.Ace, CardSuit.Clubs);
+            var result = string.Format("{0} of {1}", card.Face, card.Suit);
+            Assert.AreEqual(result, card.ToString());
+        }
+    }
+}
