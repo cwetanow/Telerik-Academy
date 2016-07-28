579b6bd200ad412fa0cffb6f44fa245cd3945529
Ivan <madritistaaa@gmail.com>
2016-07-28 18:25:09 +0300

    Start unit tests for Army of creatures

---
+++ OOP/ExamPrep/Army Of Creatures/CreaturesArmy.Test/TestCreatureFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CreaturesArmy.Test
+{
+    [TestFixture]
+    public class TestCreatureFactory
+    {
+        [Test]
+        public void TestCreateCreature_PassInvalidName_ShouldThrow()
+        {
+        }
+    }
+}
