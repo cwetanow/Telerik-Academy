59792f560987f3ff7f69a70367045aa33ebb0f7b
Ivan <madritistaaa@gmail.com>
2016-08-02 12:37:49 +0300

    Toothpaste tests

--- OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/CosmeticsFactoryTests/CreateToothpasteTests.cs
+++ OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/CosmeticsFactoryTests/CreateToothpasteTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Cosmetics.Common;
+using Cosmetics.Engine;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,61 @@ using System.Threading.Tasks;
 
 namespace Cosmetics.Tests.CosmeticsFactoryTests
 {
-    class CreateToothpasteTests
+    [TestFixture]
+    public class CreateToothpasteTests
     {
+        private static CosmeticsFactory factory = new CosmeticsFactory();
+        private static string name = "Name";
+        private static string brand = "Brand";
+        private static decimal price = 10;
+        private static GenderType gender = GenderType.Unisex;
+        private static List<string> ingredients = new List<string> { "Ingredients" };
+        //Should throw argument null by uslovie, but throws different
+        [Test]
+        public void CreateToothpaste_PassNullAsName_ShouldThrowArgumentNull()
+        {
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste(null, brand, price, gender,ingredients ));
+        }
+
+        [Test]
+        public void CreateToothpaste_PassEmptyStringAsName_ShouldNullReference()
+        {
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste(string.Empty, brand, price, gender, ingredients));
+        }
+
+        [TestCase("sh")]
+        [TestCase("too long name for product")]
+        public void CreateToothpaste_PassInvalidStringAsName_ShouldIndexOutOfRange(string namee)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateToothpaste(namee, brand, price, gender, ingredients));
+        }
+    
+
+        [Test]
+        public void CreateToothpaste_PassNullAsBrand_ShouldThrowArgumentNull()
+        {
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste(name, null, price, gender, ingredients));
+        }
+
+        [Test]
+        public void CreateToothpaste_PassEmptyStringAsBrand_ShouldNullReference()
+        {
+            Assert.Throws<NullReferenceException>(() => factory.CreateToothpaste(name, string.Empty, price, gender, ingredients));
+        }
+
+        [TestCase("s")]
+        [TestCase("too long name for product")]
+        public void CreateToothpaste_PassInvalidStringForBrand_ShouldIndexOutOfRange(string brandd)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => factory.CreateToothpaste(name, brandd, price, gender, ingredients));
+        }
+
+        [Test]
+        public void CreateToothpaste_PassValid_InitialiseCorrectly()
+        {
+            var toothpaste = factory.CreateToothpaste(name, brand, price, gender, ingredients);
+
+            Assert.AreEqual("Toothpaste", toothpaste.GetType().Name);
+        }
     }
 }
