410d36ea7a06f1c2067a36e412977ea2c4c96694
Ivan <madritistaaa@gmail.com>
2016-08-02 10:50:14 +0300

    Finish validator tests

--- OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/CommonValidatorTest.cs
+++ OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/CommonValidatorTest.cs
@@ -17,10 +17,47 @@ namespace Cosmetics.Tests
             Assert.Throws<NullReferenceException>(()=>Validator.CheckIfNull(null));
         }
 
-        [Test]
-        public void TestIfNull_PassValidValue_ShouldDoNothing()
+        [TestCase(3)]
+        [TestCase("string")]
+        public void TestIfNull_PassValidValue_ShouldNotThrow(object obj)
         {
-            Assert.DoesNotThrow(() => Validator.CheckIfNull(null));
+            Assert.DoesNotThrow(() => Validator.CheckIfNull(obj));
+        }
+
+        [Test]
+        public void TestCheckIfStringIsNullOrEmpty_PassNull_ShouldThrowNullReference()
+        {
+            Assert.Throws<NullReferenceException>(() => Validator.CheckIfStringIsNullOrEmpty(null));
+        }
+
+        [Test]
+        public void TestCheckIfStringIsNullOrEmpty_PassEmptyString_ShouldThrowNullReference()
+        {
+            Assert.Throws<NullReferenceException>(() => Validator.CheckIfStringIsNullOrEmpty(string.Empty));
+        }
+
+        [TestCase("string")]
+        public void TestCheckIfStringIsNullOrEmpty_PassValidString_ShouldNotThrow(string text)
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfStringIsNullOrEmpty(text));
+        }
+
+        [TestCase(10,4,"two")]
+        public void TestCheckIfStringLengthIsValid_PassStringWithLengthBelowMin_ShouldThrowIndexOutOFRange(int max, int min, string text)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Validator.CheckIfStringLengthIsValid(text,max,min));
+        }
+
+        [TestCase(10, 4, "four hundred seventy three")]
+        public void TestCheckIfStringLengthIsValid_PassStringWithLengthAboveMax_ShouldThrowIndexOutOFRange(int max, int min, string text)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Validator.CheckIfStringLengthIsValid(text, max, min));
+        }
+
+        [TestCase(10, 4, "sixty")]
+        public void TestCheckIfStringLengthIsValid_PassValidString_ShouldNotThrow(int max, int min, string text)
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfStringLengthIsValid(text, max, min));
         }
     }
 }
