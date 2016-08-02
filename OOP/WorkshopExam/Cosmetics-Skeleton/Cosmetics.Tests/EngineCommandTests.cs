ef8ea4e4835cb32758e2a2138ec757d9f6c8946b
Ivan <madritistaaa@gmail.com>
2016-08-02 11:52:02 +0300

    Finish command parse tests

---
+++ OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/EngineCommandTests.cs
@@ -0,0 +1,54 @@
+﻿using Cosmetics.Engine;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests
+{
+    [TestFixture]
+    public class EngineCommandTests
+    {
+        [TestCase("ValidCommand")]
+        public void TestParse_PassValidInput_ShouldReturnCommand(string commandText)
+        {
+            var command = Command.Parse(commandText);
+
+            Assert.AreEqual("Command", command.GetType().Name);
+        }
+
+        [Test]
+        public void TestParse_PassValidInput_ShouldReturnCommandWithValidName()
+        {
+            var name = "Command";
+            var parameters = new List<string>() { "ParameterOne", "ParameterTwo" };
+            var commandText = name + " " + string.Join(" ", parameters);
+
+            var command = Command.Parse(commandText);
+
+            Assert.AreEqual(command.Name, name);
+        }
+
+        [Test]
+        public void TestParse_PassValidInput_ShouldReturnCommandWithValidParameters()
+        {
+            var name = "Command";
+            var parameters = new List<string>() { "ParameterOne", "ParameterTwo" };
+            var commandText = name + " " + string.Join(" ", parameters);
+
+            var command = Command.Parse(commandText);
+
+            Assert.AreEqual(command.Parameters, parameters);
+        }
+
+        [Test]
+        public void TestParse_PassStringEmpty_ShouldThrowArgumentNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Command.Parse(string.Empty));
+        }
+
+    }
+}
