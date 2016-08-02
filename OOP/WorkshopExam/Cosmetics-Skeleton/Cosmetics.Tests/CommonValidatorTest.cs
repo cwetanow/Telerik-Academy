1151d5dac53bd1195145a1389fd01225ee25eebc
Ivan <madritistaaa@gmail.com>
2016-08-02 10:38:49 +0300

    Unit testing workshop, cosmetics shop tests

---
+++ OOP/WorkshopExam/Cosmetics-Skeleton/Cosmetics.Tests/CommonValidatorTest.cs
@@ -0,0 +1,26 @@
+﻿using Cosmetics.Common;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Tests
+{
+    [TestFixture]
+    public class CommonValidatorTest
+    {
+        [Test]
+        public void TestIfNull_PassNull_ShouldThrowNullReference()
+        {
+            Assert.Throws<NullReferenceException>(()=>Validator.CheckIfNull(null));
+        }
+
+        [Test]
+        public void TestIfNull_PassValidValue_ShouldDoNothing()
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfNull(null));
+        }
+    }
+}
