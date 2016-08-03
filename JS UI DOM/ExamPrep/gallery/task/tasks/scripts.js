a38e524454fcc65bcbb1c66fdc9ae920a63d6e4a
Ivan <madritistaaa@gmail.com>
2016-08-03 11:35:51 +0300

    Hide cats

--- JS UI DOM/ExamPrep/gallery/task/tasks/scripts.js
+++ JS UI DOM/ExamPrep/gallery/task/tasks/scripts.js
@@ -14,23 +14,30 @@
     //Fix table width
     var $width = Math.round(100/columns)-1;
     $('.gallery .image-container').width($width+'%');
+    $('.selected').hide();
 
-
+    //Change main image
     $('.image-container img').click(function(){
+      $('.selected').show();
       var $target = $(this).attr('src');
       var $prev = $(this).parent().prev().find('img').attr('src');
       var $next = $(this).parent().next().find('img').attr('src');
 
       if (!$prev) {
-        $prev=$(this).parent().last().find('img').attr('src');
+        $prev=$('.gallery-list').children().last().find('img').attr('src');
       }
       if (!$next) {
-        $prev=$(this).parent().first().find('img').attr('src');
+        $next=$('.gallery-list').children().first().find('img').attr('src');
       }
-      console.log($prev);
+
       $('.previous-image img').attr('src', $prev);
       $('.current-image img').attr('src', $target);
       $('.next-image img').attr('src', $next);
+    });
+
+    //hide selected when clicked on main
+    $('.current-image img').click(function(){
+      $('.selected').hide();
     });
 
 
