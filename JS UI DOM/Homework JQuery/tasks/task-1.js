e796e66a2a48c7251592c281eeb83d4b1a1482ed
Ivan <madritistaaa@gmail.com>
2016-08-03 13:04:52 +0300

    jquery homework task 1

---
+++ JS UI DOM/Homework JQuery/tasks/task-1.js
@@ -0,0 +1,56 @@
+/* globals $ */
+
+/*
+
+Create a function that takes a selector and COUNT, then generates inside a UL with COUNT LIs:
+  * The UL must have a class `items-list`
+  * Each of the LIs must:
+    * have a class `list-item`
+    * content "List item #INDEX"
+      * The indices are zero-based
+  * If the provided selector does not selects anything, do nothing
+  * Throws if
+    * COUNT is a `Number`, but is less than 1
+    * COUNT is **missing**, or **not convertible** to `Number`
+      * _Example:_
+        * Valid COUNT values:
+          * 1, 2, 3, '1', '4', '1123'
+        * Invalid COUNT values:
+          * '123px' 'John', {}, []
+*/
+
+function solve() {
+  return function (selector, count) {
+   if (count<1) {
+     throw Error();
+   }
+   if (!Number(count)) {
+      throw Error();
+   }
+   if (!count) {
+      throw Error();
+   }
+   if (!selector) {
+       throw Error();
+   }
+
+   if (typeof selector !=='string') {
+      throw Error();
+   }
+   count=Number(count);
+   var $element = $(selector);
+   if ($element) {
+     var $ul = $("<ul class='items-list'></ul>");
+     for (var i = 0; i < count; i+=1) {
+        var $li = $("<li class='list-item'></li>");
+        $li.html("List item #"+i);
+        $ul.append($li);
+     }
+     $element.append($ul);
+   }
+
+
+  };
+};
+
+module.exports = solve;
