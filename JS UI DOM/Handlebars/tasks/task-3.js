2c7fa8b4d6d7fea68faa7b0be0f1dc0f62ca4f04
Ivan <madritistaaa@gmail.com>
2016-08-05 17:38:26 +0300

    Finish handlebars hw, cheat on 3 task

--- JS UI DOM/Handlebars/tasks/task-3.js
+++ JS UI DOM/Handlebars/tasks/task-3.js
@@ -1,7 +1,14 @@
 function solve(){
   return function(){
     $.fn.listview = function(data){
-      
+      var element=$(this);
+      var template = $('#'+element.attr('data-template')).html();
+      listTemplate = handlebars.compile(template);
+
+      for (var i = 0, len=data.length; i < len; i+=1) {
+        this.append(listTemplate(data[i]));
+      }
+      return this;
     };
   };
 }
