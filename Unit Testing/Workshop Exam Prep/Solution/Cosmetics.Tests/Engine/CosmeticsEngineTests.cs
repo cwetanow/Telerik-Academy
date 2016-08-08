f005a39a86d8ce7df7bcdc47be8390d18b7e745e
Ivan <madritistaaa@gmail.com>
2016-08-08 19:41:10 +0300

    Me break stuff

---
+++ Unit Testing/Workshop Exam Prep/Solution/Cosmetics.Tests/Engine/CosmeticsEngineTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Cosmetics.Tests.Engine
+{
+    [TestFixture]
+    public class CosmeticsEngineTests
+    {
+        [Test]
+        public void TestStart_InputIsValidStringAddToCategory_ShouldCallAddToCategory()
+        {
+            
+        }
+    }
+}
