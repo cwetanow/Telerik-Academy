0818c6cbcb68ac0ca3c7981db2c234cb9b0544cb
Ivan <madritistaaa@gmail.com>
2016-08-08 21:42:24 +0300

    Category tests finish

---
+++ Unit Testing/Workshop Exam Prep/Solution/Cosmetics.Tests/Mocks/MockedCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmetics.Contracts;
+using Cosmetics.Products;
+
+namespace Cosmetics.Tests.Mocks
+{
+    class MockedCategory : Category
+    {
+        public MockedCategory() : base("Mocked category")
+        {
+        }
+
+        public IList<IProduct> Products
+        {
+            get { return base.products; }
+        }
+    }
+}
--- Unit Testing/Workshop Exam Prep/Solution/Cosmetics.Tests/Products/CategoryTests.cs
+++ Unit Testing/Workshop Exam Prep/Solution/Cosmetics.Tests/Products/CategoryTests.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cosmetics.Contracts;
+using Cosmetics.Tests.Mocks;
+using Moq;
+using NUnit.Framework;
 
 namespace Cosmetics.Tests.Products
 {
+    [TestFixture]
     class CategoryTests
     {
+        [Test]
+        public void TestAddProduct_ShouldAddCorrectly()
+        {
+            var mockedProduct = new Mock<IProduct>();
+            var category = new MockedCategory();
+
+            category.AddProduct(mockedProduct.Object);
+
+            Assert.IsTrue(category.Products.Contains(mockedProduct.Object));
+        }
+
+        [Test]
+        public void TestAddProduct_ShouldRemoveCorrectly()
+        {
+            var mockedProduct = new Mock<IProduct>();
+            var category = new MockedCategory();
+
+            category.AddProduct(mockedProduct.Object);
+            category.RemoveProduct(mockedProduct.Object);
+
+            Assert.IsFalse(category.Products.Contains(mockedProduct.Object));
+        }
     }
 }
