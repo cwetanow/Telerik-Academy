c29b9177f2d28930223b21c26fade02de29d821a
Ivan <madritistaaa@gmail.com>
2016-08-13 23:34:53 +0300

    Passes in node tests, fails in bgcoder

--- JS UI DOM/ExamPrep/Calendar-template/tasks/task-3.js
+++ JS UI DOM/ExamPrep/Calendar-template/tasks/task-3.js
@@ -1,8 +1,7 @@
 function solve() {
     return function (selector) {
 
-        var template =
-            '<div class="events-calendar">' +
+        var template = '<div class="events-calendar">' +
             '<h2 class="header">' +
             'Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span>' +
             '</h2>' +
@@ -11,8 +10,8 @@ function solve() {
             '<div class="date">{{day}}</div>' +
             '<div class="events">' +
             '{{#events}}' +
-            '<div class="event {{importance}}" {{#if comment}}title="{{title}}"{{/if}}>' +
-            '<div class="title">{{#if title}} {{title}} {{else}} Free slot {{/if}}</div>' +
+            '<div class="event {{importance}}" {{#if comment}}title="{{comment}}"{{/if}}>' +
+            '<div class="title">{{#if title}}{{title}}{{else}}Free slot{{/if}}</div>' +
             '{{#if time}}' +
             '<span class="time">at: {{time}}</span>' +
             '{{/if}}' +
