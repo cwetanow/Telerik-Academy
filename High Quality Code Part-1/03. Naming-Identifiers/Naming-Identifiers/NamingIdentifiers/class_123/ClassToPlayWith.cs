c4542e9e6cb02797acdc1de818e36d37f1e47df9
Ivan <madritistaaa@gmail.com>
2016-08-25 20:26:56 +0300

    Finish class_123 task and fix names

---
+++ High Quality Code Part-1/03. Naming-Identifiers/Naming-Identifiers/NamingIdentifiers/class_123/ClassToPlayWith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingIdentifiers.class_123
+{
+    class ClassToPlayWith
+    {
+        private const int MaxCount = 6;
+
+        class InnerClassToHaveMoreFun
+        {
+            public void WriteVariableAsString(bool variable)
+            {
+                var variableAsString = variable.ToString();
+                Console.WriteLine(variableAsString);
+            }
+        }
+
+        public static void EntryMethod()
+        {
+            var innerClassInstance = new InnerClassToHaveMoreFun();
+            innerClassInstance.WriteVariableAsString(true);
+        }
+    }
+}
