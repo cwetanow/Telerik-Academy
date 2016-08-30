19f107739549824811b3b38ad7c6def2e64e51c1
Ivan <madritistaaa@gmail.com>
2016-08-30 21:23:18 +0300

    Finish variables homework

---
+++ High Quality Code Part-1/05. Variables-Data-Expressions-and-Constants/HomeworkTasks/HomeworkTasks/Size.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTasks
+{
+    public class Size
+    {
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public static Size GetRotatedSize(Size s, double angleToRotate)
+        {
+            var width = Math.Abs(Math.Cos(angleToRotate)) * s.Width +
+                        Math.Abs(Math.Sin(angleToRotate)) * s.Height;
+
+            var height = Math.Abs(Math.Sin(angleToRotate)) * s.Width +
+                    Math.Abs(Math.Cos(angleToRotate)) * s.Height;
+
+            return new Size(width, height);
+        }
+    }
+}
---
+++ High Quality Code Part-1/05. Variables-Data-Expressions-and-Constants/HomeworkTasks/HomeworkTasks/StatisticsPrinter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkTasks
+{
+    public class StatisticsPrinter
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            //Simplified finding min, max and average. Why cycle when there are built in methods 
+            double max = arr.Max();
+            PrintMax(max);
+
+            var min = arr.Min();
+            PrintMin(min);
+
+            var average = arr.Average();
+            PrintAvg(average);
+        }
+
+        private void PrintMax(double max)
+        {
+
+        }
+
+        private void PrintAvg(double d)
+        {
+
+        }
+
+        private void PrintMin(double max)
+        {
+
+        }
+    }
+}
