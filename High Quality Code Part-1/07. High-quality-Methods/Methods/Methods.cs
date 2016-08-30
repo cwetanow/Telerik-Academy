73c91f70e57af0d959d8b63373bb84e5d39f5ca0
Ivan <madritistaaa@gmail.com>
2016-08-30 16:03:21 +0300

    high quality methods homework

---
+++ High Quality Code Part-1/07. High-quality-Methods/Methods/Methods.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Methods
+    {
+        public static double CalculateTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Sides should be positive");
+            }
+
+            var halfPerimeter = (a + b + c) / 2;
+            var area = Math.Sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+
+            return area;
+        }
+
+        public static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                default:
+                    throw new ArgumentException("Invalid number passed");
+            }
+        }
+
+        public static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException("Invalid elements passed");
+            }
+
+            var max = elements[0];
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i] > max)
+                {
+                    max = elements[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+
+            if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+
+            if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+        }
+
+
+        public static double CalculateDistance(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = y1.Equals(y2);
+            isVertical = x1.Equals(x2);
+
+            var distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            return distance;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(CalculateTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalculateDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            var peter = new Student
+            {
+                FirstName = "Peter",
+                LastName = "Ivanov",
+                OtherInfo = "From Sofia, born at 17.03.1992"
+            };
+
+            var stella = new Student
+            {
+                FirstName = "Stella",
+                LastName = "Markova",
+                OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993"
+            };
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
---
+++ High Quality Code Part-1/07. High-quality-Methods/Methods/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            var firstDate = ParseDate(this.OtherInfo);
+            var secondDate = ParseDate(other.OtherInfo);
+
+            return firstDate > secondDate;
+        }
+
+        private DateTime ParseDate(string input)
+        {
+            var result = DateTime.Parse(input.Substring(input.Length - 10));
+
+            return result;
+        }
+    }
+}
