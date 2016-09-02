530e31a86a4c0997c802db2f617ca985c6d49b25
Ivan <madritistaaa@gmail.com>
2016-09-02 22:03:45 +0300

    Pass 8/17

--- JavaScript-OOP/Exam-Preparation/audio-player/tasks/task-1.js
+++ JavaScript-OOP/Exam-Preparation/audio-player/tasks/task-1.js
@@ -50,20 +50,149 @@ function solve() {
             this.title = title;
             this.author = author;
         }
+
+        get id() {
+            return this._id;
+        }
+
+        set id(value) {
+            this._id = value;
+        }
+
+        get title() {
+            return this._title;
+        }
+
+        set title(value) {
+            if (!validator.validateString(value)) {
+                throw new Error();
+            }
+
+            this._title = value;
+        }
+
+        get author() {
+            return this._author;
+        }
+
+        set author(value) {
+            if (!validator.validateString(value)) {
+                throw new Error();
+            }
+
+            this._author = value;
+        }
+
+        play() {
+            let result = `${this.id}. ${this.title} - ${this.author}`;
+            return result;
+        }
+    }
+
+    class Audio extends Playable {
+        constructor(title, author, length) {
+            super(title, author);
+            this.length = length;
+        }
+
+        get length() {
+            return this._length;
+        }
+
+        set length(value) {
+            if (!validator.validateLength(value)) {
+                throw new Error();
+            }
+
+            this._length = value;
+        }
+
+        play() {
+            let result = super.play() + ` - ${this.length}`;
+            return result;
+        }
+    }
+
+    class Video extends Playable {
+        constructor(title, author, rating) {
+            super(title, author);
+            this.imdbRating = rating;
+        }
+
+        get imdbRating() {
+            return this._rating;
+        }
+
+        set imdbRating(value) {
+            if (!validator.validateImdbRating(value)) {
+                throw new Error();
+            }
+
+            this._rating = value;
+        }
+
+        play() {
+            let result = super.play() + ` - ${this.imdbRating}`;
+            return result;
+        }
+    }
+
+    class PlayList {
+        constructor(name) {
+            this.id = idGenerator.getNext();
+            this.name = name;
+            this._playables = [];
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(value) {
+            if (!validator.validateString(value)) {
+                throw new Error();
+            }
+
+            this._name = value;
+        }
+
+        addPlayable(playable) {
+            this._playables.push(playable);
+            return this;
+        }
+
+        getPlayableById(id) {
+            for (var index = 0; index < this._playables.length; index += 1) {
+                if (this._playables[index].id === id) {
+                    return this._playables[index];
+                }
+            }
+
+            return null;
+        }
+    }
+
+    class Player {
+        constructor(name) {
+            this.id = idGenerator.getNext();
+            this.name = name;
+        }
     }
 
     return {
         getPlayer: function (name) {
-
+            return new Player(name);
         },
         getPlaylist: function (name) {
+            return new PlayList(name);
         },
         getAudio: function (title, author, length) {
+            return new Audio(title, author, length);
         },
         getVideo: function (title, author, imdbRating) {
+            return new Video(title, author, imdbRating);
         }
     };
 }
-
 
 module.exports = solve;
