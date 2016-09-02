b5ee4fb97b829567cbff4bf8d0072db20f37495f
Ivan <madritistaaa@gmail.com>
2016-09-02 14:40:31 +0300

    Finish item, book and media

--- JavaScript-OOP/Exam-Preparation/academy-online-catalogs/tasks/solution.js
+++ JavaScript-OOP/Exam-Preparation/academy-online-catalogs/tasks/solution.js
@@ -1,6 +1,6 @@
 function solve() {
     let idGenerator = {
-        get: function () {
+        get: (function () {
             return (function () {
                 var lastId = 0;
                 return {
@@ -9,10 +9,10 @@ function solve() {
                     }
                 };
             } ());
-        }
+        } ())
     };
 
-    let validator = {
+    let validation = {
         constants: {
             nameConstants: {
                 nameLengthMin: 2,
@@ -33,6 +33,9 @@ function solve() {
         stringLengthValidator: function (str, min, max) {
             return str.length <= max && str.length >= min;
         },
+        stringIsExactlyLongValidator: function (str, len) {
+            return str.length === len;
+        },
         numberValidator: function (num, min, max) {
             let result;
             if (max) {
@@ -43,23 +46,148 @@ function solve() {
             return result;
         },
         stringIsNumberValidator: function (str) {
-            return isNaN(+str);
+            return !isNaN(+str);
         }
-
     };
 
-    
+    let validator = {
+        nameValidator: function (name) {
+            return validation.stringLengthValidator(name,
+                validation.constants.nameConstants.nameLengthMin,
+                validation.constants.nameConstants.nameLengthMax);
+        },
+        isbnValidator: function (isbn) {
+            return validation.stringIsExactlyLongValidator(isbn, validation.constants.bookConstants.isbnMinLength) &&
+                validation.stringIsExactlyLongValidator(isbn, validation.constants.bookConstants.isbnMaxLength) &&
+                validation.stringIsNumberValidator(isbn);
+        },
+        nonEmptyStringValidator: function (str) {
+            return str.length !== 0;
+        },
+        genreValidator: function (genre) {
+            return validation.stringLengthValidator(name,
+                validation.constants.nameConstants.genreMinLength,
+                validation.constants.nameConstants.genreMaxLength);
+        },
+        ratingValidator: function (num) {
+            return validation.numberValidator(num,
+                validation.constants.mediaConstants.ratingMin,
+                validation.constants.mediaConstants.ratingMax);
+        },
+        durationValidator: function (num) {
+            return validation.numberValidator(num,
+                validation.constants.mediaConstants.durationMinLength);
+        }
+    };
+
+    class Item {
+        constructor(name, description) {
+            this.id = idGenerator.getNext();
+            this.description = description;
+            this.name = name;
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(value) {
+            if (!validator.nameValidator(value)) {
+                throw "Name is not in the required range";
+            }
+
+            this._name = value;
+        }
+
+        get description() {
+            return this._description;
+        }
+
+        set description(value) {
+            if (!validator.nonEmptyStringValidator(value)) {
+                throw "Description cannot be empty";
+            }
+
+            this._name = value;
+        }
+    }
+
+    class Book extends Item {
+        constructor(name, isbn, genre, description) {
+            super(name, description);
+            this.isbn = isbn;
+            this.genre = genre;
+        }
+
+        get genre() {
+            return this._genre;
+        }
+
+        set genre(value) {
+            if (!validator.genreValidator(value)) {
+                throw 'Genre is not valid';
+            }
+
+            this._genre = value;
+        }
+
+        get isbn() {
+            return this._isbn;
+        }
+
+        set isbn(value) {
+            if (!validator.isbnValidator(value)) {
+                throw 'ISBN is not valid';
+            }
+
+            this._isbn = value;
+        }
+    }
+
+    class Media extends Item {
+        constructor(name, isbn, duration, rating) {
+            super(name, description);
+            this.duration = duration;
+            this.rating = rating;
+        }
+
+        get rating() {
+            return this._rating;
+        }
+
+        set rating(value) {
+            if (!validator.genreValidator(value)) {
+                throw 'Genre is not valid';
+            }
+
+            this._genre = value;
+        }
+
+        get isbn() {
+            return this._isbn;
+        }
+
+        set isbn(value) {
+            if (!validator.isbnValidator(value)) {
+                throw 'ISBN is not valid';
+            }
+
+            this._isbn = value;
+        }
+    }
 
     return {
         getBook: function (name, isbn, genre, description) {
-
+            return new Book(name, isbn, genre, description);
         },
         getMedia: function (name, rating, duration, description) {
 
         },
         getBookCatalog: function (name) {
+
         },
         getMediaCatalog: function (name) {
+
         }
     };
 }
