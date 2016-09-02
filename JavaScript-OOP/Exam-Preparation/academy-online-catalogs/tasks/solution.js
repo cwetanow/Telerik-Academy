260bc8e6202d72692f8531d4cf63a205e9e80265
Ivan <madritistaaa@gmail.com>
2016-09-02 14:14:51 +0300

    Finish validation methods

--- JavaScript-OOP/Exam-Preparation/academy-online-catalogs/tasks/solution.js
+++ JavaScript-OOP/Exam-Preparation/academy-online-catalogs/tasks/solution.js
@@ -1,5 +1,54 @@
 function solve() {
+    let idGenerator = {
+        get: function () {
+            return (function () {
+                var lastId = 0;
+                return {
+                    getNext: function () {
+                        return lastId += 1;
+                    }
+                };
+            } ());
+        }
+    };
 
+    let validator = {
+        constants: {
+            nameConstants: {
+                nameLengthMin: 2,
+                nameLengthMax: 40,
+            },
+            bookConstants: {
+                isbnMinLength: 10,
+                isbnMaxLength: 13,
+                genreMinLength: 2,
+                genreMaxLength: 20
+            },
+            mediaConstants: {
+                durationMinLength: 0,
+                ratingMin: 1,
+                ratingMax: 5
+            }
+        },
+        stringLengthValidator: function (str, min, max) {
+            return str.length <= max && str.length >= min;
+        },
+        numberValidator: function (num, min, max) {
+            let result;
+            if (max) {
+                result = num <= max && num >= min;
+            } else {
+                result = num >= min;
+            }
+            return result;
+        },
+        stringIsNumberValidator: function (str) {
+            return isNaN(+str);
+        }
+
+    };
+
+    
 
     return {
         getBook: function (name, isbn, genre, description) {
@@ -14,5 +63,5 @@ function solve() {
         }
     };
 }
-
+solve();
 module.exports = solve;
