e09ae95389c1c76e137d67db396f8243a363e7ec
Ivan <madritistaaa@gmail.com>
2016-09-02 13:56:26 +0300

    Start academy online catalog

---
+++ JavaScript-OOP/Exam-Preparation/academy-online-catalogs/tasks/solution.js
@@ -0,0 +1,18 @@
+function solve() {
+
+
+    return {
+        getBook: function (name, isbn, genre, description) {
+
+        },
+        getMedia: function (name, rating, duration, description) {
+
+        },
+        getBookCatalog: function (name) {
+        },
+        getMediaCatalog: function (name) {
+        }
+    };
+}
+
+module.exports = solve;
