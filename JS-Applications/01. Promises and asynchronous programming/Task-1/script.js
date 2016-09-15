563c0b5304b02dfa243e024a71decdd4ca0d24a5
Ivan <madritistaaa@gmail.com>
2016-09-15 19:55:41 +0300

    FInish task 1, 403 forbidden

---
+++ JS-Applications/01. Promises and asynchronous programming/Task-1/script.js
@@ -0,0 +1,33 @@
+(function () {
+    let promise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                resolve(position);
+            },
+            (error) => {
+                reject(error);
+            }
+        );
+    });
+
+    let parseCoordinates = (locationObj) => {
+
+        return {
+            lat: locationObj.coords.latitude,
+            long: locationObj.coords.longitude
+        };
+
+    };
+
+    let createImage = (locations) => {
+        let img = document.getElementById('map'),
+            imgSrc = `http://maps.googleapis.com/maps/api/staticmap?center=${locations.lat},${locations.long}&zoom=13&size=500x500&sensor=false`;
+
+        img.setAttribute('src', imgSrc);
+    };
+
+    promise
+        .then(parseCoordinates)
+        .then(createImage)
+        .catch();
+})();
