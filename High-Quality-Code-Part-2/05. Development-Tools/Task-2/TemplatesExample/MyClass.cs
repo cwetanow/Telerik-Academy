e53fb99c220e992816a4e0940c81c63f257cd4e0
Ivan <madritistaaa@gmail.com>
2016-09-28 18:09:07 +0300

    Write T4 template

---
+++ High-Quality-Code-Part-2/05. Development-Tools/Task-2/TemplatesExample/MyClass.cs
@@ -0,0 +1,57 @@
+﻿
+
+
+
+
+public class Month
+{
+
+
+    public int Sep { get; set; }
+
+
+
+    public int Oct { get; set; }
+
+
+
+    public int Nov { get; set; }
+
+
+
+    public int Dec { get; set; }
+
+
+
+    public int Jan { get; set; }
+
+
+
+    public int Feb { get; set; }
+
+
+
+    public int Mar { get; set; }
+
+
+
+    public int Apr { get; set; }
+
+
+
+    public int May { get; set; }
+
+
+
+    public int Jun { get; set; }
+
+
+
+    public int Jul { get; set; }
+
+
+
+    public int Aug { get; set; }
+
+
+}
---
+++ High-Quality-Code-Part-2/05. Development-Tools/Task-2/TemplatesExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TemplatesExample
+{
+    using System;
+    using System.IO;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var month=new Months();
+        }
+    }
+}
