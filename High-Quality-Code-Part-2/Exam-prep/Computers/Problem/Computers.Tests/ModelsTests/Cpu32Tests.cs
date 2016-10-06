ab259005553f10ec7fda06b98fd25ba9358de9fa
Ivan <madritistaaa@gmail.com>
2016-10-06 23:30:48 +0300

    Finish Cpu32 tests

---
+++ High-Quality-Code-Part-2/Exam-prep/Computers/Problem/Computers.Tests/ModelsTests/Cpu32Tests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Computers.Common.Contracts;
+using Computers.Common.Models.Cpus;
+using Computers.Common.Utils;
+using Moq;
+using NUnit.Framework;
+
+namespace Computers.Tests.ModelsTests
+{
+    [TestFixture]
+    class Cpu32Tests
+    {
+        [TestCase(-1)]
+        [TestCase(-123)]
+        [TestCase(-1432)]
+        public void TestSquareNumber_PassLowerNumber_ShouldReturnCorrectString(int number)
+        {
+            var mockedMotherboard = new Mock<IMotherboard>();
+
+            var cpu = new Cpu32(8);
+
+            cpu.AttachToMotherboard(mockedMotherboard.Object);
+
+            mockedMotherboard.Setup(x => x.LoadRamValue()).Returns(number);
+
+            cpu.SquareNumber();
+
+            mockedMotherboard.Verify(x => x.DrawOnVideoCard(GlobalConstants.NumberTooLowMessage), Times.Once);
+        }
+
+        [TestCase(1000)]
+        [TestCase(6123)]
+        [TestCase(1432)]
+        public void TestSquareNumber_PassHigherNumber_ShouldReturnCorrectString(int number)
+        {
+            var mockedMotherboard = new Mock<IMotherboard>();
+
+            var cpu = new Cpu32(8);
+
+            cpu.AttachToMotherboard(mockedMotherboard.Object);
+
+            mockedMotherboard.Setup(x => x.LoadRamValue()).Returns(number);
+
+            cpu.SquareNumber();
+
+            mockedMotherboard.Verify(x => x.DrawOnVideoCard(GlobalConstants.NumberTooHighMessage), Times.Once);
+        }
+
+        [TestCase(100)]
+        [TestCase(212)]
+        [TestCase(14)]
+        public void TestSquareNumber_PassCorrectNumber_ShouldReturnCorrectString(int number)
+        {
+            var mockedMotherboard = new Mock<IMotherboard>();
+
+            var cpu = new Cpu32(8);
+
+            cpu.AttachToMotherboard(mockedMotherboard.Object);
+
+            mockedMotherboard.Setup(x => x.LoadRamValue()).Returns(number);
+
+            var expectedString = $"Square of {number} is {number * number}.";
+
+            cpu.SquareNumber();
+
+            mockedMotherboard.Verify(x => x.DrawOnVideoCard(expectedString), Times.Once);
+        }
+
+        [TestCase(100, 2022)]
+        [TestCase(11, 212)]
+        [TestCase(14, 15)]
+        public void TestGenerateRandom_ShouldCallSaveToRam(int lower, int higher)
+        {
+            var mockedMotherboard = new Mock<IMotherboard>();
+
+            var cpu = new Cpu32(8);
+
+            cpu.AttachToMotherboard(mockedMotherboard.Object);
+
+            cpu.GenerateRandom(lower, higher);
+
+            mockedMotherboard.Verify(x => x.SaveRamValue(It.IsAny<int>()), Times.Once);
+        }
+    }
+}
