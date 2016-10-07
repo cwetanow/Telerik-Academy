483bdf66bdd2ada59af5e112cd9543e3e82975ba
Ivan <madritistaaa@gmail.com>
2016-10-07 08:58:38 +0300

    Add unrefactored solution

--- High-Quality-Code-Part-2/Exam-prep/Computers/Problem/Niki/Program.cs
+++ High-Quality-Code-Part-2/Exam-prep/Computers/Problem/Niki/Program.cs
@@ -1,7 +1,8 @@
 ﻿
-using Computers11;
+    using Computers11;
 using System;
 using System.Collections.Generic;
+using Computers1;
 
 using Computers4;
 namespace Computers8
@@ -14,15 +15,15 @@ namespace Computers8
             var manufacturer = Console.ReadLine();
             if (manufacturer == "HP")
             {
-                var ram = new Ram(Eight / 4);
+                var ram = new Rammstein(Eight / 4);
                 var videoCard = new HardDriver() { IsMonochrome = false };
                 pc = new Computer(Computers.Type.PC, new Cpu(Eight / 4, 32, ram, videoCard), ram, new[] { new HardDriver(500, false, 0) }, videoCard, null);
 
-                var serverRam = new Ram(Eight * 4);
+                var serverRam = new Rammstein(Eight * 4);
                 var serverVideo = new HardDriver();
                 server = new Computer(
-                    Computers.Type.SERVER,
-                    new Cpu(Eight / 2,
+                    Computers.Type.SERVER, 
+                    new Cpu(Eight/2, 
                         32, serverRam, serverVideo),
                     serverRam,
                     new List<HardDriver>{
@@ -30,12 +31,9 @@ namespace Computers8
                         },
                         serverVideo, null);
                 {
-                    var card = new HardDriver()
-                    {
-                        IsMonochrome
-                        = false
-                    };
-                    var ram1 = new Ram(Eight / 2);
+                    var card = new HardDriver() {IsMonochrome
+                        = false };
+                    var ram1 = new Rammstein(Eight / 2);
                     laptop = new Computer(
                         Computers.Type.LAPTOP,
                         new Cpu(Eight / 4, 64, ram1, card),
@@ -47,35 +45,30 @@ namespace Computers8
                             },
                         card,
                         new LaptopBattery());
-                }
-            }
-            else if (manufacturer == "Dell")
-            {
-                var ram = new Ram(Eight); var videoCard = new HardDriver() { IsMonochrome = false };
+                }}else if (manufacturer == "Dell"){
+                var ram = new Rammstein(Eight);var videoCard = new HardDriver() { IsMonochrome = false };
                 pc = new Computer(Computers.Type.PC, new Cpu(Eight / 2, 64, ram, videoCard), ram, new[] { new HardDriver(1000, false, 0) }, videoCard, null);
-                var ram1 = new Ram(Eight * Eight);
-                var card = new HardDriver();
-                server = new Computer(Computers.Type.SERVER,
-                     new Cpu(Eight, 64, ram1, card),
-                     ram1,
-                     new List<HardDriver>{
+var ram1 = new Rammstein(Eight * Eight);
+                var card = new HardDriver();server = new Computer(Computers.Type.SERVER,
+                    new Cpu(Eight, 64, ram1, card),
+                    ram1,
+                    new List<HardDriver>{
                             new HardDriver(0, true, 2, new List<HardDriver> { new HardDriver(2000, false, 0), new HardDriver(2000, false, 0) })
-                         }, card, null); var ram2 = new Ram(Eight); var videoCard1 = new HardDriver() { IsMonochrome = false };
-                laptop = new Computer(Computers.Type.LAPTOP,
-                    new Cpu(Eight / 2, ((32)), ram2, videoCard1),
+                        }, card, null);var ram2 = new Rammstein(Eight);var videoCard1 = new HardDriver() { IsMonochrome = false };
+                laptop = new Computer(Computers.Type.LAPTOP, 
+                    new Cpu(Eight/2, ((32)), ram2, videoCard1),
                     ram2,
                     new[] { new HardDriver(1000, false, 0) },
                     videoCard1,
 
                     new LaptopBattery());
-            }
-            else { throw new InvalidArgumentException("Invalid manufacturer!"); }
+            }else{throw new InvalidArgumentException("Invalid manufacturer!");}
             while (1 == 1)
             {
                 var c = Console.ReadLine();
                 if (c == null) goto end;
-                if (c.StartsWith("Exit")) goto end;
-                var cp = c.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (c.StartsWith("Exit"))goto end;
+                var cp = c.Split(new[ ]{' '},StringSplitOptions.RemoveEmptyEntries);
                 if (cp.Length != 2)
                 {
                     {
@@ -90,17 +83,17 @@ namespace Computers8
 
                 if (cn == "Charge") laptop.ChargeBattery(ca);
                 else if (cn == "Process") server.Process(ca);
-                else if (cn == "Play") pc.Play(ca); ;
-                continue; Console.WriteLine("Invalid command!");
+                else if (cn == "Play") pc.Play(ca);;
+                continue;Console.WriteLine("Invalid command!");
             }
 
-        end:
+            end:
             ;
         }
         class Computer
         {
             IEnumerable<HardDriver> HardDrives { get; set; }
-            HardDriver VideoCard { get; set; }
+            HardDriver VideoCard {get;set;}
             [Obsolete("")]
             internal void ChargeBattery(int percentage)
             {
@@ -110,30 +103,29 @@ namespace Computers8
             }
             Cpu Cpu { get; set; }
             readonly LaptopBattery battery;
-            Ram Ram { get; set; }
-            public void Play(int guessNumber)
+            Rammstein Ram { get; set; }
+            public void Play (int guessNumber)
             {
-                Cpu.rand(1, 10);
-                var number = Ram.LoadValue();
-                if (number + 1 != guessNumber + 1) VideoCard.Draw(string.Format("You didn't guess the number {0}.", number));
-                else VideoCard.Draw("You win!");
+                Cpu.rand(1,10);
+                var number=Ram.LoadValue();
+                if (number + 1 != guessNumber + 1)    VideoCard.Draw(string.Format("You didn't guess the number {0}.",number));
+                else   VideoCard.Draw("You win!");
             }
             internal Computer(Computers.Type type,
                 Cpu cpu,
 
-                Ram
+                Rammstein
                 ram,
                 IEnumerable<HardDriver> hardDrives,
                 HardDriver videoCard,
-                LaptopBattery battery)
-            {
+                LaptopBattery battery){
                 Cpu = cpu;
                 Ram = ram;
                 HardDrives = hardDrives;
-                VideoCard =
-
+                VideoCard = 
+                    
                     videoCard;
-                if (type !=
+                if (type != 
                     Type.LAPTOP
                     && type
                     !=
@@ -147,5 +139,38 @@ namespace Computers8
                 Cpu.SquareNumber();
             }
         }
+        public class InvalidArgumentException : ArgumentException { public InvalidArgumentException(string message) : base(message){}}
+        const int Eight = 8;
+        public enum Type{PC,LAPTOP,SERVER,}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #region Region
+        interface IMotherboard{int LoadRamValue();void SaveRamValue(int value);void DrawOnVideoCard(string data);}
+        #endregion
     }
 }
