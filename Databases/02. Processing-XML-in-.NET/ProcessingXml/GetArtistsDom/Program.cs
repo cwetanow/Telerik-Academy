519d0efa352dd4655d9831009ea7059d539f49c2
Ivan <madritistaaa@gmail.com>
2016-10-17 00:37:57 +0300

    Finish first task of processing xml homework

---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/GetArtistsDom/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Xml;
+
+namespace GetArtistsDom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            var catalog = doc.DocumentElement;
+
+            var uniqueArtists = ExtractArtists(catalog);
+
+            foreach (var artist in uniqueArtists.Keys)
+            {
+                var oneOrMany = (int)uniqueArtists[artist] > 1 ? "albums" : "album";
+                var artistAndAlbums = $"{artist}: {uniqueArtists[artist]} {oneOrMany}";
+                Console.WriteLine(artistAndAlbums);
+            }
+        }
+
+        public static Hashtable ExtractArtists(XmlElement node)
+        {
+            var tagName = "album";
+            var artists = new Hashtable();
+
+            var albums = node.GetElementsByTagName(tagName);
+
+            foreach (XmlNode album in albums)
+            {
+                var currentArtistName = album["artist"].InnerText;
+
+                if (artists.ContainsKey(currentArtistName))
+                {
+                    artists[currentArtistName] = (int)artists[currentArtistName] + 1;
+                }
+                else
+                {
+                    artists[currentArtistName] = 1;
+                }
+            }
+
+            return artists;
+        }
+    }
+}
---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/GetArtistsXPath/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GetArtistsXPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            var catalog = doc.DocumentElement;
+
+            var uniqueArtists = ExtractArtists(catalog);
+
+            foreach (var artist in uniqueArtists.Keys)
+            {
+                var oneOrMany = (int)uniqueArtists[artist] > 1 ? "albums" : "album";
+                var artistAndAlbums = $"{artist}: {uniqueArtists[artist]} {oneOrMany}";
+                Console.WriteLine(artistAndAlbums);
+            }
+        }
+
+        public static Hashtable ExtractArtists(XmlElement catalog)
+        {
+            var artistsPath = "catalogue/album/artist";
+
+            var artists = new Hashtable();
+
+            return artists;
+        }
+    }
+}
