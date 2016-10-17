f369eca37bb47e878e699581938ec81098da90a0
Ivan <madritistaaa@gmail.com>
2016-10-17 11:54:04 +0300

    Finish extracting tile with xdocument

---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/ExtractTitlesXDocument/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ExtractTitlesXDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            var doc = XDocument.Load(path);
+
+            var titles = doc.Root.Descendants("title")
+                .Select(x => x.Value);
+
+            foreach (var title in titles)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
