ad015c15a39afd5543eb4c021bb917c296e10c8f
Ivan <madritistaaa@gmail.com>
2016-10-17 18:43:05 +0300

    Add other tasks for xml processing

---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/08.CreateAlbums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+
+namespace _08.CreateAlbums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            // Creates the file int the main directory of the homework
+            var newFilePath = "../../../../albums.xml";
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+
+            using (var reader = XmlReader.Create(path))
+            {
+                using (var writer = new XmlTextWriter(newFilePath, encoding))
+                {
+                    writer.Formatting = Formatting.Indented;
+                    writer.IndentChar = '\t';
+                    writer.Indentation = 1;
+
+                    writer.WriteStartDocument();
+                    writer.WriteStartElement("Albums");
+
+                    while (reader.Read())
+                    {
+                        if ((reader.NodeType == XmlNodeType.Element) &&
+                             (reader.Name == "name"))
+                        {
+                            writer.WriteStartElement("album");
+                            writer.WriteElementString("name", reader.ReadElementString());
+                        }
+                        else if ((reader.NodeType == XmlNodeType.Element) &&
+                           (reader.Name == "artist"))
+                        {
+                            writer.WriteElementString("artist", reader.ReadElementString());
+                            writer.WriteEndElement();
+                        }
+                    }
+
+                    writer.WriteEndDocument();
+                }
+            }
+        }
+    }
+}
---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/11.ExtractAlbumsOlder5YearsXpath/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace _11.ExtractAlbumsOlder5YearsXpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+
+        }
+    }
+}
