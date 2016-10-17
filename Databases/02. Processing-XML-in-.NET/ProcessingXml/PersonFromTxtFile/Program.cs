613ddf8608c9151095a7069e0490c15b2e000821
Ivan <madritistaaa@gmail.com>
2016-10-17 15:32:32 +0300

    Finish read person from txt file task

---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/PersonFromTxtFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Xml.Linq;
+
+namespace PersonFromTxtFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../phonebook.txt";
+
+            var name = string.Empty;
+            var address = string.Empty;
+            var phone = string.Empty;
+
+            using (var reader = new StreamReader(path))
+            {
+                name = reader.ReadLine();
+                address = reader.ReadLine();
+                phone = reader.ReadLine();
+            }
+
+            var personElement = new XElement("person",
+                new XElement("name", name),
+                new XElement("address", address),
+                new XElement("phoneNumber", phone)
+                );
+
+            personElement.Save("../../../../person.xml");
+        }
+    }
+}
