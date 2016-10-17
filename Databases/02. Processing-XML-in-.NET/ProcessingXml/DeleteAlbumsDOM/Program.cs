62e6900207b7030be35cc51afe6c2999dc7ff724
Ivan <madritistaaa@gmail.com>
2016-10-17 11:23:49 +0300

    Finish delete albums task

---
+++ Databases/02. Processing-XML-in-.NET/ProcessingXml/DeleteAlbumsDOM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace DeleteAlbumsDOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "../../../../catalogue.xml";
+
+            var doc = new XmlDocument();
+            doc.Load(path);
+
+            var costyAlbums = doc.GetElementsByTagName("album")
+                .Cast<XmlNode>()
+                .Where(x => double.Parse(x["price"].InnerText) > 20);
+
+            foreach (XmlNode costyAlbum in costyAlbums)
+            {
+                doc.RemoveChild(costyAlbum);
+            }
+        }
+    }
+}
