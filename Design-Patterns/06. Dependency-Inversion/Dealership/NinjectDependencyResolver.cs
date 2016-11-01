62c42f31b55dc3ed834fe8e72b80d242f3203c0c
Ivan <madritistaaa@gmail.com>
2016-11-01 19:20:49 +0200

    Install ninject

---
+++ Design-Patterns/06. Dependency-Inversion/Dealership/NinjectDependencyResolver.cs
@@ -0,0 +1,18 @@
+﻿using Dealership.Contracts;
+using Dealership.Core;
+using Dealership.Engine;
+using Ninject.Modules;
+
+namespace Dealership
+{
+    public class NinjectDependencyResolver : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IEngine>().To<DealershipEngine>();
+
+            Bind<IWriter>().To<ConsoleInterface>();
+            Bind<IReader>().To<ConsoleInterface>();
+        }
+    }
+}
