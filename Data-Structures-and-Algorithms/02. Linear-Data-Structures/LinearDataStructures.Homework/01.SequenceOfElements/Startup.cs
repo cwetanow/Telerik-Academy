4c33b21a7acc3b56ca6b95c49cdca1d44ae4294e
Ivan <madritistaaa@gmail.com>
2016-11-16 12:17:49 +0200

    Finish first task

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/01.SequenceOfElements/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SequenceOfElements
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var elements = new List<int>();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                var element = int.Parse(input);
+                elements.Add(element);
+
+                input = Console.ReadLine();
+            }
+
+            var sum = elements.Sum();
+            var average = elements.Average();
+            Console.WriteLine($"Sum of elements is: {sum} \nAverage of elements is: {average}");
+        }
+    }
+}
