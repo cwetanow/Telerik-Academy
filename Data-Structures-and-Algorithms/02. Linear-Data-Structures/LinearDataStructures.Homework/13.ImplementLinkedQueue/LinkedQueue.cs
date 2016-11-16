3312177d8435ff33e47d093ef7f719f63adbcab6
Ivan <madritistaaa@gmail.com>
2016-11-16 13:46:53 +0200

    Implement queue with linked list

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/13.ImplementLinkedQueue/LinkedQueue.cs
@@ -0,0 +1,73 @@
+﻿namespace _13.ImplementLinkedQueue
+{
+    public class LinkedQueue<T>
+    {
+        protected class Node
+        {
+            public Node(T obj, Node prev)
+            {
+                this.Element = obj;
+                this.Next = null;
+
+                prev.Next = this;
+            }
+
+            public Node(T obj)
+            {
+                this.Element = obj;
+                this.Next = null;
+            }
+
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node headNode;
+        private Node tailNode;
+
+        public LinkedQueue()
+        {
+            this.headNode = null;
+            this.tailNode = this.headNode;
+
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public void Enqueue(T element)
+        {
+            if (this.IsEmpty())
+            {
+                this.headNode = new Node(element);
+                this.tailNode = this.headNode;
+            }
+            else
+            {
+                this.tailNode = new Node(element, this.tailNode);
+            }
+
+            this.Count++;
+        }
+
+        public T Dequeue()
+        {
+            var element = this.headNode.Element;
+
+            this.headNode = this.headNode.Next;
+
+            return element;
+        }
+
+        public T Peek()
+        {
+            return this.headNode.Element;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/13.ImplementLinkedQueue/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _13.ImplementLinkedQueue
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new LinkedQueue<int>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
