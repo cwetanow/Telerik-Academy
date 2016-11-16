d8da5bc8bad6c6ad33c118c783d717aa392bb753
Ivan <madritistaaa@gmail.com>
2016-11-16 12:24:11 +0200

    Add sort elements task

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/03.SortElements/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.SortElements
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var elements = new List<int>();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                var element = int.Parse(input);
+                elements.Add(element);
+
+                input = Console.ReadLine();
+            }
+
+            elements.Sort();
+
+            Console.WriteLine("Sorted elements");
+            foreach (var element in elements)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
