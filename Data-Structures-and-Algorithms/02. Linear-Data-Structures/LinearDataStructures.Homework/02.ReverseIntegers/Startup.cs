9b530c73c6b4079357e8f41cd6633eb4c3d86af8
Ivan <madritistaaa@gmail.com>
2016-11-16 12:21:34 +0200

    Add reverse elements task

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/02.ReverseIntegers/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.ReverseIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of elements");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            var stack = new Stack<int>();
+            for (var i = 0; i < n; i++)
+            {
+                var element = int.Parse(Console.ReadLine());
+                stack.Push(element);
+            }
+
+            Console.WriteLine("\nReversed elements:");
+            for (var i = 0; i < n; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
