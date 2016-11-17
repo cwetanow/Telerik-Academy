b30160e31c8560cbbcee40805c6c645ce1a5650d
Ivan <madritistaaa@gmail.com>
2016-11-17 17:03:08 +0200

    Add logic for getting genres

--- Web-Applications-with-Node.js/Workshops/ImdbScrapper/app.js
+++ Web-Applications-with-Node.js/Workshops/ImdbScrapper/app.js
@@ -9,14 +9,6 @@ const htmlParser = require('./utils/html-parser');
 const queueFactory = require('./data-structures/queue');
 let urlQueue = queueFactory.getQueue();
 
-constants.genres.forEach(function (genre) {
-    for (let index = 0; index < constants.pagesCount; index += 1) {
-        let url = `http://www.imdb.com/search/title?genres=${genre}&title_type=feature&sort=moviemeter,asc&page=${index+1}&view=simple&ref_=adv_nxt`;
-
-        urlQueue.enqueue(url);
-    }
-}, this);
-
 function wait(time) {
     return new Promise((resolve, reject) => {
         setTimeout(function () {
@@ -33,7 +25,7 @@ db.on('error', (err) => {
     console.log(err);
 });
 
-function getMoviesFromUrl(url) {
+function getMoviesFromUrl(url, queue) {
     httpRequester.get(url)
         .then((result) => {
             const selector = '.col-title span[title] a';
@@ -52,11 +44,11 @@ function getMoviesFromUrl(url) {
             return wait(2000);
         })
         .then(() => {
-            if (urlQueue.isEmpty()) {
+            if (queue.isEmpty()) {
                 return;
             }
 
-            getMoviesFromUrl(urlQueue.dequeue());
+            getMoviesFromUrl(queue.dequeue(), queue);
         })
         .catch((err) => {
             console.dir(err, {
@@ -65,12 +57,48 @@ function getMoviesFromUrl(url) {
         });
 }
 
-const asyncPagesCount = 30;
+function getGenres() {
+    httpRequester.get('http://www.imdb.com/genre/')
+        .then((result) => {
+            const selector = '.genre-table h3 a';
+            const html = result.body;
+
+            return htmlParser.parseSimpleMovie(html, selector);
+        })
+        .then((imdbGenres) => {
+            imdbGenres.forEach(function (genre) {
+                genre.title = genre.title.split(' ')[0].toLowerCase();
+            }, this);
+
+            console.log('Received genres');
+
+            imdbGenres.forEach(function (genre) {
+                for (let index = 0; index < constants.pagesCount; index += 1) {
+                    let url = `http://www.imdb.com/search/title?genres=${genre.title}&title_type=feature&sort=moviemeter,asc&page=${index+1}&view=simple&ref_=adv_nxt`;
+
+                    urlQueue.enqueue(url);
+                }
+            }, this);
+
+            return Promise.resolve()
+                .then(() => {
+                    return urlQueue;
+                });
+        })
+        .then((queue) => {
+            const asyncPagesCount = 30;
+
+            for (let i = 0; i < asyncPagesCount; i += 1) {
+                getMoviesFromUrl(queue.dequeue(), queue);
+            }
+
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
 
 db.on('open', () => {
     console.log('Connection is open');
-
-    for (let i = 0; i < asyncPagesCount; i += 1) {
-        getMoviesFromUrl(urlQueue.dequeue());
-    }
+    getGenres();
 }, this);
