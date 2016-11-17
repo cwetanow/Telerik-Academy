eba14955ce9bc7428856931774d09188ee9dbe30
Ivan <madritistaaa@gmail.com>
2016-11-17 22:07:37 +0200

    Finish remove numbers that occur odd number of times task

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/06.RemoveNumbersOddTimes/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.RemoveNumbersOddTimes
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+
+            var timesCount = new Dictionary<int, int>();
+
+            foreach (var element in list)
+            {
+                if (timesCount.ContainsKey(element))
+                {
+                    timesCount[element]++;
+                }
+                else
+                {
+                    timesCount[element] = 1;
+                }
+            }
+
+            foreach (var count in timesCount)
+            {
+                if (count.Value % 2 == 1)
+                {
+                    list.RemoveAll(x => x == count.Key);
+                }
+            }
+
+            Console.WriteLine("Elements left");
+            foreach (var element in list)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
