02b7827b5497f413d97c3ee90a94ae4a91bc3e8c
Ivan <madritistaaa@gmail.com>
2016-11-17 11:12:01 +0200

    Implement linked list

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/11.LinkedListImplementation/LinkedList.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Text;
+
+namespace _11.LinkedListImplementation
+{
+    public class LinkedList<T>
+        where T : IComparable
+    {
+        private class Node
+        {
+            public Node(T obj)
+            {
+                this.Element = obj;
+                this.Next = null;
+            }
+
+            public Node(T obj, Node previousNode)
+            {
+                this.Element = obj;
+                this.Next = null;
+
+                previousNode.Next = this;
+            }
+
+            public T Element { get; set; }
+
+            public Node Next { get; set; }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void Add(T obj)
+        {
+            if (this.IsEmpty())
+            {
+                this.head = new Node(obj);
+                this.tail = this.head;
+            }
+            else
+            {
+                this.tail = new Node(obj, this.tail);
+            }
+
+            this.Count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+
+                var current = this.head;
+
+                for (var i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+
+                return current.Element;
+            }
+            set
+            {
+                this.ValidateIndex(index);
+
+                var current = this.head;
+                for (var i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+
+                current.Element = value;
+            }
+        }
+
+        public T ElementAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            return this[index];
+        }
+
+        public T RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+            T result;
+
+            if (index == 0)
+            {
+                result = this.head.Element;
+                this.head = this.head.Next;
+
+                return result;
+            }
+            else
+            {
+                Node previousNode = null;
+                var current = this.head;
+
+                for (var i = 0; i < index; i++)
+                {
+                    previousNode = current;
+                    current = current.Next;
+                }
+
+                result = current.Element;
+
+                previousNode.Next = current.Next;
+            }
+
+            return result;
+        }
+
+        public T Remove(T obj)
+        {
+            for (var i = 0; i < this.Count; i++)
+            {
+                if (this.ElementAt(i).Equals(obj))
+                {
+                    return this.RemoveAt(i);
+                }
+            }
+
+            throw new ArgumentNullException("Invalid object");
+        }
+
+        public bool Contains(T obj)
+        {
+            return this.IndexOf(obj) != -1;
+        }
+
+        public int IndexOf(T obj)
+        {
+            var index = 0;
+            var current = this.head;
+
+            while (current != null)
+            {
+                if (current.Element.Equals(obj))
+                {
+                    return index;
+                }
+
+                index++;
+                current = current.Next;
+            }
+
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            for (int i = 0; i < this.Count - 1; i++)
+            {
+                result.Append(string.Format($"{this[i]}->"));
+            }
+
+            result.Append(this[this.Count - 1]);
+
+            return result.ToString();
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException("Index out of range");
+            }
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/11.LinkedListImplementation/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _11.LinkedListImplementation
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+
+            Console.WriteLine("Contains 3? -> " + list.Contains(3));
+
+            Console.WriteLine(list);
+        }
+    }
+}
