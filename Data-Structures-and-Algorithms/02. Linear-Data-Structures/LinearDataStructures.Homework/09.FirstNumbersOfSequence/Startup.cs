37504d4d1b009d059667a1e6f3db46b6f66b8538
Ivan <madritistaaa@gmail.com>
2016-11-18 15:13:07 +0200

    Finish linear data structures homework

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/09.FirstNumbersOfSequence/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.FirstNumbersOfSequence
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var queue = new Queue<int>();
+
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            queue.Enqueue(n);
+
+            for (var i = 0; i < 50; i++)
+            {
+                Console.WriteLine(queue.Peek());
+
+                var s = queue.Dequeue();
+
+                queue.Enqueue(s + 1);
+                queue.Enqueue(2 * s + 1);
+                queue.Enqueue(s + 2);
+            }
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/LinearDataStructures.Homework/10.SortedOperators/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.SortedOperators
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            var start = 5; // int.Parse(Console.ReadLine());
+            var end = 16; // int.Parse(Console.ReadLine());
+
+            var sequence = ShortestSequenceOfOperations(end, start);
+
+            Console.WriteLine(sequence);
+        }
+
+        private static string ShortestSequenceOfOperations(int end, int start)
+        {
+            var list = new List<int> { end };
+
+            while (end > start)
+            {
+                if (end / 2 >= start)
+                {
+                    if (end % 2 == 1)
+                    {
+                        end--;
+                        list.Add(end);
+                    }
+
+                    end /= 2;
+                    list.Add(end);
+                }
+                else if (end - 2 >= start)
+                {
+                    end -= 2;
+                    list.Add(end);
+                }
+                else
+                {
+                    end--;
+                    list.Add(end);
+                }
+            }
+
+            list.Sort();
+
+            return string.Join(", ", list);
+        }
+    }
+}
