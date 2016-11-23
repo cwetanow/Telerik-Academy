1decc09011363120d8cbd4990ac282fdd1b2bfa2
Ivan <madritistaaa@gmail.com>
2016-11-23 23:04:10 +0200

    Start recursion homework

---
+++ Data-Structures-and-Algorithms/03. Recursion/RecursionHomework/01.NestedLoops/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.NestedLoops
+{
+    public class Startup
+    {
+        private static int n;
+
+        public static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+
+            Loops(0);
+        }
+
+        public static void Loops(int index)
+        {
+
+        }
+
+        public static void InnerLoop()
+        {
+
+        }
+    }
+
+}
+
+}
