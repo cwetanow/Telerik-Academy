ae1adf7fda98c02a0499d59b8a7dbbf7183c4833
Ivan <madritistaaa@gmail.com>
2016-12-13 12:15:43 +0200

    Implement trie and solve task for homework

---
+++ Data-Structures-and-Algorithms/Structures/DataStructures/DataStructures.Playground/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace DataStructures.Playground
+{
+    class Startup
+    {
+
+        static void Main(string[] args)
+        {
+            var dictionary = new[] { "algorithm          10",
+                "selection sort    20",
+                "linked list           20",
+                "program            5",
+                "viagra                -100" };
+
+            var words = "The selection sort algorithm could be applied to linked lists.";
+
+            var trie = new Trie();
+
+            foreach (var s in dictionary)
+            {
+                var input = s.Split(' ')
+                    .Where(x => !string.IsNullOrEmpty(x))
+                    .ToArray();
+
+                var result = string.Empty;
+
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    result += (input[i] + ' ');
+                }
+
+                trie.AddWord(result.ToLower(), int.Parse(input[input.Length - 1]));
+            }
+
+            var current = string.Empty;
+            var score = 0;
+            var spaceCounters = 0;
+
+            words = words.ToLower();
+            for (var i = 0; i < words.Length; i++)
+            {
+                current += words[i];
+
+                if (words[i] == ' ')
+                {
+                    spaceCounters++;
+
+                    if (!trie.HasWord(current))
+                    {
+                        current = string.Empty;
+                    }
+                }
+
+                if (trie.GetWordWeight(current) != 0)
+                {
+                    if (!char.IsLetter(words[i + 1]))
+                    {
+                        score += trie.GetWordWeight(current);
+                        current = string.Empty;
+                    }
+                }
+            }
+
+            Console.WriteLine(score / spaceCounters);
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/Structures/DataStructures/DataStructures/Trie.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataStructures
+{
+    public class Trie
+    {
+        private class Node
+        {
+            public Node()
+            {
+                this.Children = new List<Node>();
+            }
+
+            public Node(char character, int weight = 0)
+            {
+                this.Character = character;
+                this.Weight = weight;
+                this.Children = new List<Node>();
+            }
+
+            public IList<Node> Children { get; private set; }
+
+            public char Character { get; private set; }
+
+            public int Weight { get; private set; }
+
+            public Node FindChild(char c)
+            {
+                for (int i = 0; i < this.Children.Count; i++)
+                {
+                    if (Children.ElementAt(i).Character == c)
+                    {
+                        return Children.ElementAt(i);
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        private Node root;
+
+        public Trie()
+        {
+            this.root = new Node();
+        }
+
+        public void AddWord(string word, int weight)
+        {
+            if (word.Length == 0)
+            {
+                return;
+            }
+
+            var current = this.root;
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                var currentCharacter = word[i];
+                var child = current.FindChild(currentCharacter);
+
+                if (child != null)
+                {
+                    current = child;
+                }
+                else
+                {
+                    Node newNode;
+
+                    if (i == word.Length - 2)
+                    {
+                        newNode = new Node(currentCharacter, weight);
+                    }
+                    else
+                    {
+                        newNode = new Node(currentCharacter);
+                    }
+
+                    current.Children.Add(newNode);
+                    current = newNode;
+                }
+            }
+        }
+
+        public bool HasWord(string word)
+        {
+            var current = root;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var child = current.FindChild(word[i]);
+
+                if (child != null)
+                {
+                    current = child;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public int GetWordWeight(string word)
+        {
+            if (this.HasWord(word))
+            {
+                var current = this.root;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    var child = current.FindChild(word[i]);
+
+                    if (child != null)
+                    {
+                        current = child;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+
+                return current.Weight;
+            }
+
+            return 0;
+        }
+    }
+}
