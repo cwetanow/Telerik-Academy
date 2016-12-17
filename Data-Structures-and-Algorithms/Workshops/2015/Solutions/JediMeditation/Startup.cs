3923cfbf9df92b92c4d74ab79449fc5140f8b107
Ivan <madritistaaa@gmail.com>
2016-12-17 14:27:56 +0200

    Workshop task jedi meditation, zero tests pass, others exception

---
+++ Data-Structures-and-Algorithms/Workshops/2015/Solutions/JediMeditation/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JediMeditation
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var jedis = Console.ReadLine()
+                .Split(' ');
+
+            var dictionary = new Dictionary<char, List<string>>();
+
+            dictionary['m'] = new List<string>();
+            dictionary['k'] = new List<string>();
+            dictionary['p'] = new List<string>();
+
+            foreach (var jedi in jedis)
+            {
+                dictionary[jedi[0]].Add(jedi);
+            }
+
+            var result = new StringBuilder();
+            foreach (var jedi in dictionary)
+            {
+                result.Append(string.Join(" ", jedi.Value) + ' ');
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
