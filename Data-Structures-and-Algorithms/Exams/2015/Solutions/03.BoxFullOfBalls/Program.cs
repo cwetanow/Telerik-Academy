96dd5415e42dfb6056027bd8e234188d54810ba7
Ivan <madritistaaa@gmail.com>
2017-01-07 19:47:32 +0200

    Start task 3

---
+++ Data-Structures-and-Algorithms/Exams/2015/Solutions/03.BoxFullOfBalls/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.BoxFullOfBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var possibleTurns = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            var ballsInterval = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var a = ballsInterval[0];
+            var b = ballsInterval[1];
+            var gamesPlayed = b - a + 1;
+
+            var mikisWins = 0;
+
+            for (var i = a; i <= b; i++)
+            {
+                
+            }
+
+            Console.WriteLine(mikisWins);
+        }
+    }
+}
