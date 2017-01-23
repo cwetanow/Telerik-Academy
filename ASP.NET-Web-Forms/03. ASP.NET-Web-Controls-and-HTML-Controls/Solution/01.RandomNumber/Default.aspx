7d68f23b3f58840064e8e21f4e147744c2ce8cb6
Ivan <madritistaaa@gmail.com>
2017-01-23 12:07:41 +0200

    Add first task

---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-Controls-and-HTML-Controls/Solution/01.RandomNumber/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_01.RandomNumber._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>With HTML controls</h1>
+        <input type="number" id="MinNumberInput" runat="server" />
+        <input type="number" id="MaxNumberInput" runat="server" />
+        <br />
+        <input type="button" runat="server" onserverclick="OnButtonRandomClick" value="Generate" />
+        <hr />
+        <span runat="server" id="Sum"></span>
+    </div>
+    <div class="jumbotron">
+        <h1>With HTML controls</h1>
+        <asp:TextBox runat="server" ID="MinInputServer"></asp:TextBox>
+        <asp:TextBox runat="server" ID="MaxInputServer"></asp:TextBox>
+        <br />
+        <asp:Button runat="server" OnClick="OnServerButtonClick" Text="Generate" />
+        <hr />
+        <asp:Label runat="server" ID="ServerRandomResult"></asp:Label>
+    </div>
+</asp:Content>
---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-Controls-and-HTML-Controls/Solution/01.RandomNumber/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _01.RandomNumber
+{
+    public partial class _Default : Page
+    {
+        private static Random random = new Random();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OnButtonRandomClick(object sender, EventArgs e)
+        {
+            var min = 0;
+            var first = int.TryParse(this.MinNumberInput.Value, out min);
+
+            var max = 0;
+            var second = int.TryParse(this.MaxNumberInput.Value, out max);
+
+            if (first && second)
+            {
+                var num = random.Next(min, max + 1);
+                this.Sum.InnerText = num.ToString();
+            }
+            else
+            {
+                this.Sum.InnerText = "Invalid input";
+            }
+        }
+
+        protected void OnServerButtonClick(object sender, EventArgs e)
+        {
+            var min = 0;
+            var first = int.TryParse(this.MinInputServer.Text, out min);
+
+            var max = 0;
+            var second = int.TryParse(this.MaxInputServer.Text, out max);
+
+            if (first && second)
+            {
+                var num = random.Next(min, max + 1);
+                this.ServerRandomResult.Text = num.ToString();
+            }
+            else
+            {
+                this.ServerRandomResult.Text = "Invalid input";
+            }
+        }
+    }
+}
